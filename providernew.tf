82b0a368f13d4c162d0515ac1bec5139db0ae8cb
Ramanjulur <ramanjulureddy.p@gmail.com>
2020-06-02 20:20:44 +0530

    Update providernew.tf

--- providernew.tf
+++ providernew.tf
@@ -1,7 +1,7 @@
 #Creates a VPC with cidr block 10.0.0.0/16
 provider "aws" {
-  access_key = "AKIA4HU2WTDB2FREWW3P"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
 
